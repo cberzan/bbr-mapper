18403d3068e692bfb79c4a51682f153544483d38
Constantin Berzan <cberzan@gmail.com>
2010-10-19 20:57:57 -0400

    extract all rectangles from SVG

--- svg2sim/svg2cfg.py
+++ svg2sim/svg2cfg.py
@@ -15,12 +15,27 @@ def svg_size(svgdom):
     height = float(attrs["height"].value)
     return (width, height)
 
+def svg_rectangles(svgdom):
+    """
+    Returns list of (x, y, width, height) tuples representing rectangles
+    extracted from given SVG DOM tree.
+    """
+    rects = []
+    for svgrect in svgdom.getElementsByTagName("rect"):
+        x      = float(svgrect.attributes["x"].value)
+        y      = float(svgrect.attributes["y"].value)
+        width  = float(svgrect.attributes["width"].value)
+        height = float(svgrect.attributes["height"].value)
+        rects.append((x, y, width, height))
+    return rects
+
 if __name__=="__main__":
     if len(sys.argv) < 2:
         print "Usage: %s file.xml\nWill produce file.cfg" % sys.argv(0)
         exit(1)
     svgdom        = minidom.parse(sys.argv[1])
     width, height = svg_size(svgdom)
-    #rectangles    = svg_rects(svgdom)
-    print "Width %lf, height %lf, %d rectangles." % (width, height, 0)
+    rectangles    = svg_rectangles(svgdom)
+    print "Width %lf, height %lf, %d rectangles." %\
+            (width, height, len(rectangles))
     pass
