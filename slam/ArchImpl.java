fadd6cef4aa8fbd98f52139a596b2a8ee69bf206
Constantin Berzan <cberzan@gmail.com>
2010-11-11 16:49:01 -0500

    can't sleep too much in the constructor

--- slam/ArchImpl.java
+++ slam/ArchImpl.java
@@ -25,10 +25,20 @@ public class ArchImpl extends ActionServerImpl implements Arch {
      * Moves ahead stopDist meters with speed tSpeed, then stops.
      * Prints out odometry and global pose at each step.
      */
-    public void runArchitecture()
-    {
+    public void runArchitecture() {
+        while(ekfServer == null) {
+            ekfServer = getClient("com.slam.EKFServer");
+            if(ekfServer != null)
+                break;
+            System.out.println("ArchImpl: waiting for EKFServer.");
+            //return;
+            Sleep(200);
+        }
+
+        System.out.println("ArchImpl: ekfServer is " + ekfServer);
+
         try {
-            Pose pose = (Pose)call(ekfServer, "getPose");
+            OurPose pose = (OurPose)call(ekfServer, "getPose");
             System.out.println("Pose: " + pose);
         } catch(Exception e) {
             System.err.println("Error getting pose from EKFServer: " + e);
@@ -44,12 +54,5 @@ public class ArchImpl extends ActionServerImpl implements Arch {
     public ArchImpl() throws RemoteException
     {
         super();
-        while(ekfServer == null) {
-            ekfServer = getClient("com.slam.EKFServer");
-            if(ekfServer != null)
-                break;
-            System.out.println("ArchImpl: waiting for EKFServer.");
-            Sleep(200);
-        }
     }
 }
