231bae8df2f93fcf9e96f1b9c3c3f0e9cc4a8a7f
Constantin Berzan <cberzan@gmail.com>
2010-11-11 16:17:25 -0500

    make EKFServer.getPose() public

--- slam/EKFServer.java
+++ slam/EKFServer.java
@@ -7,5 +7,5 @@ import java.rmi.*;
 
 public interface EKFServer extends ADEServer {
     /// Get current pose as predicted by EKF.
-    Pose getPose() throws RemoteException;
+    public Pose getPose() throws RemoteException;
 }
