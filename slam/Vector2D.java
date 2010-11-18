90940ee023fb1447685c5f2518eb70ce5aafea98
Constantin Berzan <cberzan@gmail.com>
2010-11-17 21:53:13 -0500

    fix some serious bugs in Vector2D, write a test
    * mag, dir need to be private if sanity is to be preserved
    * need to be careful when modifying mag, dir, because they are used in
      the calculation of x and y. For example, mag = [expr]; dir = [expr] is
      wrong if [expr] contains getX() or getY().
    
    This is why I said it's better to use cartesian -- so much easier to
    think about.

--- slam/Vector2D.java
+++ slam/Vector2D.java
@@ -4,13 +4,13 @@ import java.io.Serializable;
 
 /**
  * Holds a 2D vector.
- * Dir is in radians.
+ * Dir is in radians, from 0 to 2pi.
  */
 public class Vector2D implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    public double mag;
-    public double dir; // rad
+    private double mag;
+    private double dir; // rad
 
     public Vector2D() {
         mag = dir = 0;
@@ -33,19 +33,15 @@ public class Vector2D implements Serializable {
     }
 
     public void setX(double x) {
-        mag = Math.sqrt(x * x + getY() * getY());
-        dir = myatan2(getY(), x);
-        
+        setPol(Math.sqrt(x * x + getY() * getY()), myatan2(getY(), x));
     }
 
     public void setY(double y) {
-        mag = Math.sqrt(getX() * getX() + y * y);
-        dir = myatan2(y, getX());
+        setPol(Math.sqrt(getX() * getX() + y * y), myatan2(y, getX()));
     }
 
     public void setCart(double x, double y) {
-        setX(x);
-        setY(y);
+        setPol(Math.sqrt(x * x + y * y), myatan2(y, x));
     }
 
     public void setMag(double mag0) {
@@ -58,28 +54,75 @@ public class Vector2D implements Serializable {
     }
 
     public void setPol(double mag0, double dir0) {
-        setMag(mag0);
-        setDir(dir0);
+        mag = mag0;
+        dir = dir0;
     }
 
     public Vector2D plus(Vector2D other) {
         Vector2D result = new Vector2D();
-        result.setX(getX() + other.getX());
-        result.setY(getY() + other.getY());
+        result.setCart(getX() + other.getX(), getY() + other.getY());
         return result;
     }
 
     public Vector2D mul(double value) {
         Vector2D result = new Vector2D();
-        mag *= value;
+        result.setPol(getMag() * value, getDir());
         return result;
     }
 
     /// atan2(y, x) returning [0, 2pi]
-    private double myatan2(double y, double x) {
+    static private double myatan2(double y, double x) {
         double result = Math.atan2(y, x);
         if(result < 0)
             result += 2 * Math.PI;
         return result;
+    }
+
+    static public void test() throws Exception {
+        double mag1 = 0.076, dir1 = 195.5 * Math.PI / 180;
+        double x1 = mag1 * Math.cos(dir1), y1 = mag1 * Math.sin(dir1);
+        Vector2D v1 = new Vector2D();
+        v1.setMag(mag1);
+        v1.setDir(dir1);
+        Util.assert_feq(v1.getMag(), mag1);
+        Util.assert_feq(v1.getDir(), dir1);
+        Util.assert_feq(v1.getX(), x1);
+        Util.assert_feq(v1.getY(), y1);
+
+        double mag2 = 1.0, dir2 = 275.8 * Math.PI / 180;
+        double x2 = mag2 * Math.cos(dir2), y2 = mag2 * Math.sin(dir2);
+        Vector2D v2 = new Vector2D();
+        v2.setMag(mag2);
+        v2.setDir(dir2);
+        Util.assert_feq(v2.getMag(), mag2);
+        Util.assert_feq(v2.getDir(), dir2);
+        Util.assert_feq(v2.getX(), x2);
+        Util.assert_feq(v2.getY(), y2);
+
+        double sumx = x1 + x2, sumy = y1 + y2;
+        double summag = Math.sqrt(sumx * sumx + sumy * sumy),
+               sumdir_trig = Math.atan2(sumy, sumx),
+               sumdir = myatan2(sumy, sumx);
+        Util.assert_feq(Math.sin(sumdir_trig), Math.sin(sumdir));
+        Util.assert_feq(Math.cos(sumdir_trig), Math.cos(sumdir));
+
+        Vector2D sum = new Vector2D();
+        Util.assert_feq(sum.getMag(), 0);
+        Util.assert_feq(sum.getDir(), 0);
+        Util.assert_feq(sum.getX(), 0);
+        Util.assert_feq(sum.getY(), 0);
+        sum = sum.plus(v1);
+        sum = sum.plus(v2);
+        Util.assert_feq(sum.getX(), sumx);
+        Util.assert_feq(sum.getY(), sumy);
+        Util.assert_feq(sum.getMag(), summag);
+        Util.assert_feq(sum.getDir(), sumdir);
+
+        double factor = 123;
+        sum = sum.mul(factor);
+        Util.assert_feq(sum.getX(), sumx * factor);
+        Util.assert_feq(sum.getY(), sumy * factor);
+        Util.assert_feq(sum.getMag(), summag * factor);
+        Util.assert_feq(sum.getDir(), sumdir); // unchanged
     }
 };
