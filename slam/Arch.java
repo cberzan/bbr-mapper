d38316b7f5556282039862c0196b331a1f675ff9
Constantin Berzan <cberzan@gmail.com>
2010-11-24 11:31:13 -0500

    make getBeaconData() public in Arch
    This picks up the definition of getBeaconData() in ActionServerImpl. I'm
    not sure why this works, but it seems to work...

--- slam/Arch.java
+++ slam/Arch.java
@@ -7,4 +7,6 @@ package com.slam;
 import java.rmi.RemoteException;
 import ade.ADEServer;
 import com.action.ActionServer;
-public interface Arch extends ActionServer {}
+public interface Arch extends ActionServer {
+    public Double[][] getBeaconData() throws RemoteException;
+}
