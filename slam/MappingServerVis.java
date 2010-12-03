443bf007dc370bdbe6da39f67765735b0e50731b
Constantin Berzan <cberzan@gmail.com>
2010-12-02 21:30:55 -0500

    draw robot

--- slam/MappingServerVis.java
+++ slam/MappingServerVis.java
@@ -5,6 +5,7 @@ package com.slam;
 
 import ade.*;
 import java.awt.*;
+import java.awt.geom.*;
 import java.rmi.*;
 import javax.swing.*;
 
@@ -51,9 +52,39 @@ public class MappingServerVis extends ADEGuiPanel
                 }
                 float component = (float)(1.0 - sum / maxSum);
                 g.setColor(new Color(component, component, component));
-                g.fillRect(sx, sy, 1, 1); // FIXME really? no putPixel??
+                g.fillRect(sx, ssize.height - sy - 1, 1, 1); // FIXME really? no putPixel??
             }
         }
+
+        // Draw robot.
+        final int robotRadius   = 10,
+                  headingRadius = 15;
+        Pose pose = data.robotPose;
+        Point2D.Double robotCenter = new Point2D.Double(pose.x, pose.y);
+        Point robotCenterS  = map2screen(data.world2map(robotCenter)),
+              robotHeadingS = new Point(
+                robotCenterS.x + (int)(headingRadius * Math.cos(pose.theta)),
+                robotCenterS.y - (int)(headingRadius * Math.sin(pose.theta)));
+        g.setColor(Color.red);
+        g.drawOval(robotCenterS.x - robotRadius, robotCenterS.y - robotRadius,
+                   robotRadius * 2, robotRadius * 2);
+        g.drawLine(robotCenterS.x, robotCenterS.y,
+                   robotHeadingS.x, robotHeadingS.y);
+        System.out.format("robotCenter: world %s map %s screen %s\n",
+                robotCenter, data.world2map(robotCenter),
+                map2screen(data.world2map(robotCenter)));
+    }
+
+    /// Converts from map coordinates to screen coordinates.
+    Point map2screen(Point onMap) {
+        Dimension ssize     = getSize();
+        Dimension msize     = new Dimension(data.map[0].length, data.map.length);
+        double mPixPerSPixX = 1.0 * msize.width / ssize.width;
+        double mPixPerSPixY = 1.0 * msize.height / ssize.height;
+        Point onScreen      = new Point();
+        onScreen.x          = (int)((onMap.x / mPixPerSPixX));
+        onScreen.y          = ssize.height - (int)((onMap.y / mPixPerSPixY)) - 1;
+        return onScreen;
     }
 
     @Override
--- slam/MappingServerVisData.java
+++ slam/MappingServerVisData.java
@@ -3,6 +3,7 @@
 package com.slam;
 
 import java.awt.geom.Point2D;
+import java.awt.Point;
 
 /// Holds all the visualization data necessary required by MappingServerVis.
 public class MappingServerVisData {
@@ -11,4 +12,13 @@ public class MappingServerVisData {
     public Point2D.Double mapCenter;
     public double mapPixPerMeter;
     public byte[][] map;
+    public Pose robotPose;
+
+    /// Converts from world coordinates to map coordinates.
+    Point world2map(Point2D.Double onWorld) {
+        Point onMap = new Point();
+        onMap.x = (int)((onWorld.x - mapMin.x) * mapPixPerMeter);
+        onMap.y = (int)((onWorld.y - mapMin.y) * mapPixPerMeter);
+        return onMap;
+    }
 }
